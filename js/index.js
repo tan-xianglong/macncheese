eb220c0bb02431d7ab5590e7c6d95660879d0116
tan-xianglong <t.xianglong@gmail.com>
2022-02-10 13:28:05 +0800

    added submitForm function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//initialize DOM elements for form items
+
 const chkbox_beef = document.querySelector("#chkbox_beef");
 const chkbox_seafood = document.querySelector("#chkbox_seafood");
 const chkbox_chicken = document.querySelector("#chkbox_chicken");
@@ -9,6 +11,12 @@ const pdtQuantity = document.querySelector("#quantity");
 const pdtImg = document.querySelector("image");
 const clearBtn = document.querySelector("#clearBtn");
 
+// Initialize global variables
+let checkboxChecked;
+let isCheckBoxChecked = false;
+// Initialize macNcheese product
+const macNcheese = new Product();
+
 // 1. Checkbox restriction
 //    a. create DOM variables for all checkboxes
 //    b. create event handler
@@ -17,6 +25,9 @@ const clearBtn = document.querySelector("#clearBtn");
 //event handler
 const disableChkbox = () => {
   if (chkbox_all.checked == true) {
+    chkbox_beef.checked = false;
+    chkbox_chicken.checked = false;
+    chkbox_seafood.checked = false;
     chkbox_beef.disabled = true;
     chkbox_chicken.disabled = true;
     chkbox_seafood.disabled = true;
@@ -43,18 +54,36 @@ const clearForm = () => {
   pdtImg.reset();
 };
 
-//storing form values
-let productName = pdtNameField.value;
-let productDesc = pdtDesc.value;
-let price = pdtPrice.value;
-let quantity = pdtQuantity.value;
-let catBeef = chkbox_beef.value;
-let catSeafood = chkbox_seafood.value;
-let catChicken = chkbox_chicken.value;
-
+//3. submit form function to validate and store values
+const submitForm = () => {
+  //form validation
+  if (!isCheckBoxChecked){
+    chkbox_beef.setCustomValidity("Please select at least one category.");
+    chkbox_beef.reportValidity();
+  } else {
+    //initialize storing form values variables
+    let productName = pdtNameField.value;
+    let productDesc = pdtDesc.value;
+    let price = pdtPrice.value;
+    let quantity = pdtQuantity.value;
+    let catBeef = chkbox_beef.value;
+    let catSeafood = chkbox_seafood.value;
+    let catChicken = chkbox_chicken.value;
+    let createAt = new Date();
+    macNcheese.addProduct(
+      productName,
+      productDesc,
+      price,
+      quantity,
+      category,
+      image,
+      createAt
+    ); //category and image to work on it
+    alert("Product has been added successfully.");
+  }
+};
 
 //dummy data for productItem array
-const macNcheese = new Product();
 macNcheese.addProduct(
   "Original Mac N Cheese",
   "Our best seller, nothing beats the original",
